36361b7d86dbfcfb6d55618fb5dbd91aaa95f2f2
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-04-12 13:12:01 +0100

    feat: add FilePathValidator to exports in eos_validators module

--- pytk/etc/eos/eos_validators/__init__.py
+++ pytk/etc/eos/eos_validators/__init__.py
@@ -2,11 +2,13 @@ from .ping import PingValidator
 from .version import VersionValidator
 from .processor import numProcessors, Processor_Info
 from .user import UserValidator
+from .show import FilePathValidator
 
 __all__ = [
     "PingValidator",
     "VersionValidator",
     "numProcessors",
     "Processor_Info",
-    "UserValidator"
-    ]
+    "UserValidator",
+    "FilePathValidator",
+]
