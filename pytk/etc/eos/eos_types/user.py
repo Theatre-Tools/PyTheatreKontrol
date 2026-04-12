7bca634bec89c28d827fec01d8e517a3cf5efc7b
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-04-12 13:14:21 +0100

    refactor: fix indentation and formatting in user.py

--- pytk/etc/eos/eos_types/user.py
+++ pytk/etc/eos/eos_types/user.py
@@ -4,16 +4,16 @@ from typing import List
 
 @dataclass
 class User:
-	"""Represents a single user connected to the Eos show file."""
+    """Represents a single user connected to the Eos show file."""
 
-	id: int
-	console_type: str
-	console_name: str
+    id: int
+    console_type: str
+    console_name: str
 
 
 @dataclass
 class UserListResponse:
-	"""Represents the parsed response from /eos/out/get/userlist."""
- 
-	users: List[User]
-	count: int
+    """Represents the parsed response from /eos/out/get/userlist."""
+
+    users: List[User]
+    count: int
