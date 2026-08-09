8c7fb09ac5f05aae6bf43ecb8a6f0bcf41a46cf5
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-08-09 19:39:19 +0100

    fix: update cueControl protocol to use string type for cue parameters, this is due to cue numbers, while being numbers to the user, shouldn't be treated as number by code.

--- pytk/lighting/cueControl.py
+++ pytk/lighting/cueControl.py
@@ -4,19 +4,18 @@ from typing import Protocol
 class cueControl(Protocol):
     """A protocol for controlling the cue state of a device."""
 
-    async def goto_cue(self, cue: int | float) -> None:
+    async def goto_cue(self, cue: str) -> None:
         """Go to a specific cue."""
         ...
 
-    async def record_cue(self, cue: int | float) -> None:
+    async def record_cue(self, cue: str) -> None:
         """Record a specific cue."""
         ...
 
-    async def delete_cue(self, cue: int | float) -> None:
+    async def delete_cue(self, cue: str) -> None:
         """Delete a specific cue."""
         ...
 
-    async def update_cue(self, cue: int | float) -> None:
+    async def update_cue(self, cue: str) -> None:
         """Update a specific cue."""
         ...
-
