8a16e0074dbde356ff0f21351b929b1538308636
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-01 00:21:12 +0100

    feat: refactor Eos class to remove inheritance from playbackControl and update connect/disconnect methods to synchronous

--- pytk/drivers/etc/eos/eos.py
+++ pytk/drivers/etc/eos/eos.py
@@ -2,12 +2,19 @@ from pyosc import ConnectionRole, OSCFraming, OSCMessage, OSCString, OSCTranspor
 
 from pytk.core.device import Device
 from pytk.lighting.control.cueControl import cueControl
-from pytk.lighting.control.playbackControl import playbackControl
 
 from .eosPlaybackControl import EosPlaybackControl
+from .eosPlaybackHandler import eosPlaybackHandler
+from .eosPlaybackTypes import (
+    eosActiveCueCompletionValidator,
+    eosActiveCueValidator,
+    eosCuePendingValidator,
+    eosPlaybackEventValidator,
+    eosPlaybackStates,
+)
 
 
-class Eos(Device, playbackControl):
+class Eos(Device):
     """A device that implements the Eos protocol."""
 
     def __init__(
@@ -26,12 +33,18 @@ class Eos(Device, playbackControl):
             framing=OSCFraming.OSC11,
         )
         self.cues = EosPlaybackControl(self)
+        self.playback = eosPlaybackStates()
+        self.eos_playback_handler = eosPlaybackHandler(self)
 
-    async def connect(self) -> None:
+    def connect(self) -> None:
         """Connect to the Eos device."""
+        self.conn.register_handler(message_address="/eos/out/active/cue/*/*", validator=eosActiveCueValidator, func=self.eos_playback_handler.handle_playback_event)
+        self.conn.register_handler(message_address="/eos/out/active/cue", validator=eosActiveCueCompletionValidator, func=self.eos_playback_handler.handle_playback_event)
+        self.conn.register_handler(message_address="/eos/out/cue/pending/*/*", validator=eosCuePendingValidator, func=self.eos_playback_handler.handle_playback_event)
+        self.conn.register_handler(message_address="/eos/out/event/cue/*/*/*", validator=eosPlaybackEventValidator, func=self.eos_playback_handler.handle_playback_event)
         self.conn.start_listening()
 
-    async def disconnect(self) -> None:
+    def disconnect(self) -> None:
         """Disconnect from the Eos device."""
         self.conn.stop_listening()
 
