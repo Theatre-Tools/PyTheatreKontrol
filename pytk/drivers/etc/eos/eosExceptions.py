b89acfd4404107041916510b075d79bad9f7fe4b
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-08 16:48:02 +0100

    fix: add EosConnectionError exception for handling connection errors

--- pytk/drivers/etc/eos/eosExceptions.py
+++ pytk/drivers/etc/eos/eosExceptions.py
@@ -1,7 +1,15 @@
+from pyosc import Exceptions
+
 from pytk.core.exceptions import DriverError
 
 
 class EosSyntaxError(DriverError):
     """Raised when a command sent to the Eos device has a syntax error."""
+
+    pass
+
+
+class EosConnectionError(Exceptions.SocketError):
+    """Raised when there is a connection error with the Eos device."""
 
     pass
