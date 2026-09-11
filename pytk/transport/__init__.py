be0dd5a6e892a1285aa8dcee3af6f5d805da6a83
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-11 22:07:58 +0100

    feat: add SerialTCP to transport module exports

--- pytk/transport/__init__.py
+++ pytk/transport/__init__.py
@@ -1,3 +1,4 @@
 from .osc import OSC, OSC10, OSC11, OSCUDP
+from .serial import SerialTCP
 
-__all__ = ["OSC", "OSC10", "OSC11", "OSCUDP"]
+__all__ = ["OSC", "OSC10", "OSC11", "OSCUDP", "SerialTCP"]
