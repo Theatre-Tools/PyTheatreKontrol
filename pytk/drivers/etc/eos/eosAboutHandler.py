e006218c2221bb22b1ec22f4f40ffce104fd227b
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-24 19:13:00 +0100

    feat: extend eos_about_handler to support eosShowSaveEventValidator

--- pytk/drivers/etc/eos/eosAboutHandler.py
+++ pytk/drivers/etc/eos/eosAboutHandler.py
@@ -1,18 +1,22 @@
-from .eosEventValidators import eosLockEventValidator
+from datetime import datetime
+from .eosEventValidators import eosLockEventValidator, eosShowSaveEventValidator
 from .eosAboutValidators import eosVersionValidator
 
-from Typing import TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from pyosc import OSCMessage
 
 if TYPE_CHECKING:
     from .eos import Eos
 
+
 class eosAboutHandler:
     def __init__(self, eos: Eos):
         self.eos = eos
 
-    def eos_about_handler(self, message: eosVersionValidator | eosLockEventValidator | OSCMessage) -> None:
+    def eos_about_handler(
+        self, message: eosVersionValidator | eosLockEventValidator | eosShowSaveEventValidator | OSCMessage
+    ) -> None:
         """Handles all utility based eos info endpoints by default for the eos.about property"""
         if isinstance(message, eosVersionValidator):
             self.eos.about.software_version = message.software_version
@@ -20,10 +24,22 @@ class eosAboutHandler:
             self.eos.about.gel_swatch_type = message.gel_swatch_type
         elif isinstance(message, eosLockEventValidator):
             self.eos.about.locked = message.locked
+        elif isinstance(message, eosShowSaveEventValidator):
+            self.eos.about.show_file = message.show_file
+            self.eos.about.last_saved = datetime.now()
+
         else:
             raise ValueError(f"Unexpected message type: {type(message)}")
 
+
 def register_about_handlers(eos: Eos):
     """Registers the about handlers for the eos device"""
-    eos.conn.register_handler(message_address="/eos/out/get/version", func=eos._about_handler.eos_about_handler, validator=eosVersionValidator)
-    eos.conn.register_handler(message_address="/eos/out/lock", func=eos._about_handler.eos_about_handler, validator=eosLockEventValidator)
+    eos.conn.register_handler(
+        message_address="/eos/out/get/version", func=eos._about_handler.eos_about_handler, validator=eosVersionValidator
+    )
+    eos.conn.register_handler(
+        message_address="/eos/out/lock", func=eos._about_handler.eos_about_handler, validator=eosLockEventValidator
+    )
+    eos.conn.register_handler(
+        message_address="/eos/out/event/show/*", func=eos._about_handler.eos_about_handler, validator=eosShowSaveEventValidator
+    )
