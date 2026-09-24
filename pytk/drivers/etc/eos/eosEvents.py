9cb4802b05cc2463a38071ca73cb9f743b0b3867
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-24 21:13:39 +0100

    fix: enhance type handling in eosEvents class event handler

--- pytk/drivers/etc/eos/eosEvents.py
+++ pytk/drivers/etc/eos/eosEvents.py
@@ -1,6 +1,10 @@
-from typing import Callable
+from typing import Callable, TypeVar
+
+from pydantic import BaseModel
 
 from .eventDefenitions import Event
+
+T = TypeVar("T", bound=BaseModel)
 
 
 class eosEvents:
@@ -13,12 +17,13 @@ class eosEvents:
         """Registers an event handler for the given event type."""
         self.eos.conn.register_handler(message_address=event_type.address, validator=event_type.validator, func=handler)
 
-    def handler(self, event: Event) -> Callable[[Callable[[Event], None]], Callable[[Event], None]]:
+    def handler(self, event: Event[T]) -> Callable[[Callable[[T], None]], Callable[[T], None]]:
         """Returns a callable that can be used to register an event handler for the given event type."""
 
-        def handler_decorator(func: Callable[[Event], None]) -> Callable[[Event], None]:
+        def handler_decorator(func: Callable[[T], None]) -> Callable[[T], None]:
             """A callable that can be used to register an event handler for the given event type."""
             self.eos.conn.register_handler(message_address=event.address, validator=event.validator, func=func)
 
             return func
+
         return handler_decorator
