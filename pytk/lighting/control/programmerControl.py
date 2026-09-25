e8da3bdfa55391eb85b1eeaeaf9400978a673ad0
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-25 18:30:40 +0100

    chore: update comments in programmerControl protocol for clarity on future enhancements

--- pytk/lighting/control/programmerControl.py
+++ pytk/lighting/control/programmerControl.py
@@ -3,3 +3,6 @@ from typing import Protocol
 
 class programmerControl(Protocol):
     """A protocol for controlling the device's programmer."""
+
+    ## This has been left intentionally vauge for now, once I have a better understanding of what feaures are common between desks
+    ## this will become populated with more specific methods.
