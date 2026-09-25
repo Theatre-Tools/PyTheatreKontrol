b35770c16a1c9123538d79785e418a14d20f751c
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-25 15:28:07 +0100

    fix: remove async from set_level and set_value methods in Address protocol

--- pytk/lighting/device/channel.py
+++ pytk/lighting/device/channel.py
@@ -4,6 +4,6 @@ from typing import Protocol
 class Channel(Protocol):
     """A protocol for controlling a device's channel."""
 
-    async def intensity(self, value: float) -> None:
+    def intensity(self, value: float) -> None:
         """Set the intensity of the channel."""
         ...
